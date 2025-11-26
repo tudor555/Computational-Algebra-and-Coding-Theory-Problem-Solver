acb61a19f9d552800843937469829e638cc2c875
Tudor_Bumb <bumbtudor03@gmail.com>
2025-11-26 21:31:42 +0200

    feat: generate code parity check component

---
+++ src/app/pages/problem11-code-parity-check/problem11-code-parity-check.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem11-code-parity-check',
+  imports: [],
+  templateUrl: './problem11-code-parity-check.html',
+  styleUrl: './problem11-code-parity-check.scss',
+})
+export class Problem11CodeParityCheck {
+
+}
