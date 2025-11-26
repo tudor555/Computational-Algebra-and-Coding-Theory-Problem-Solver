e841277f0c213529dfc3cc052379bc0dce75f12f
Tudor_Bumb <bumbtudor03@gmail.com>
2025-11-26 21:34:10 +0200

    feat: generate zn order iedmpotent component

---
+++ src/app/pages/problem6-zn-order-idempotent/problem6-zn-order-idempotent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem6-zn-order-idempotent',
+  imports: [],
+  templateUrl: './problem6-zn-order-idempotent.html',
+  styleUrl: './problem6-zn-order-idempotent.scss',
+})
+export class Problem6ZnOrderIdempotent {
+
+}
