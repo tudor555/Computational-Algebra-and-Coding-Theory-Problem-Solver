fa8304df6bad7f77832dcc8f0258c3fda8feb131
Tudor_Bumb <bumbtudor03@gmail.com>
2025-11-26 21:31:08 +0200

    feat: generate matrix decomposition component

---
+++ src/app/pages/problem2-matrix-decomposition/problem2-matrix-decomposition.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem2-matrix-decomposition',
+  imports: [],
+  templateUrl: './problem2-matrix-decomposition.html',
+  styleUrl: './problem2-matrix-decomposition.scss',
+})
+export class Problem2MatrixDecomposition {
+
+}
