295b59c8722bebdd5b7b781519ffb4fc34aaa967
Tudor_Bumb <bumbtudor03@gmail.com>
2025-12-03 20:53:55 +0200

    feat: add component for euclid continued fraction problem

---
+++ src/app/pages/problem14-euclid-continued-fraction/problem14-euclid-continued-fraction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem14-euclid-continued-fraction',
+  imports: [],
+  templateUrl: './problem14-euclid-continued-fraction.html',
+  styleUrl: './problem14-euclid-continued-fraction.scss',
+})
+export class Problem14EuclidContinuedFraction {
+
+}
