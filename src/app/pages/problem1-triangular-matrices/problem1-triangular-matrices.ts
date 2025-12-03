74f65301ede466cd22c0b2e1cf8e3eafacb80071
Tudor_Bumb <bumbtudor03@gmail.com>
2025-12-03 20:53:15 +0200

    feat: add component for triangular matrices problem

---
+++ src/app/pages/problem1-triangular-matrices/problem1-triangular-matrices.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem1-triangular-matrices',
+  imports: [],
+  templateUrl: './problem1-triangular-matrices.html',
+  styleUrl: './problem1-triangular-matrices.scss',
+})
+export class Problem1TriangularMatrices {
+
+}
