b74d4653c4088d92d22a8db1d9f11b78caba383f
Tudor_Bumb <bumbtudor03@gmail.com>
2025-12-03 21:10:14 +0200

    feat: add routes for new problems

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,24 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Problem1TriangularMatrices } from './pages/problem1-triangular-matrices/problem1-triangular-matrices';
 import { Problem2MatrixDecomposition } from './pages/problem2-matrix-decomposition/problem2-matrix-decomposition';
 import { Problem5GaussRankInverse } from './pages/problem5-gauss-rank-inverse/problem5-gauss-rank-inverse';
 import { Problem6ZnOrderIdempotent } from './pages/problem6-zn-order-idempotent/problem6-zn-order-idempotent';
 import { Problem11CodeParityCheck } from './pages/problem11-code-parity-check/problem11-code-parity-check';
 import { Problem12Eigenvalues } from './pages/problem12-eigenvalues/problem12-eigenvalues';
+import { Problem14EuclidContinuedFraction } from './pages/problem14-euclid-continued-fraction/problem14-euclid-continued-fraction';
 import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
   { path: 'home', component: Home },
+  { path: 'triangular-matrices', component: Problem1TriangularMatrices },
   { path: 'matrix-decomposition', component: Problem2MatrixDecomposition },
   { path: 'gauss-rank-inverse', component: Problem5GaussRankInverse },
   { path: 'zn-order-idempotent', component: Problem6ZnOrderIdempotent },
   { path: 'code-parity-check', component: Problem11CodeParityCheck },
   { path: 'eigen-values', component: Problem12Eigenvalues },
+  { path: 'euclid-continued-fraction', component: Problem14EuclidContinuedFraction },
   { path: '**', component: NotFound }, // Wildcard route for 404
   { path: 'not-found', component: NotFound }, // Wildcard route for 404
 ];
