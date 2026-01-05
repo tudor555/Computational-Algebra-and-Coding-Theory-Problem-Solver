a48c092f97fbe43219de4c1c2188073f6a99a905
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-05 14:26:50 +0200

    feat: add base design for footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('computational-algebra-and-coding-theory-problem-solver');
