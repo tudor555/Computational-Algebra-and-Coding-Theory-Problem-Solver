38e7236a651b7482905e641800ac1eef0c236a59
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-07 19:25:06 +0200

    feat: add support for multilanguage text

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {
   ArrowLeft,
@@ -16,6 +22,7 @@ import {
   LayoutGrid,
 } from 'lucide-angular/src/icons';
 import { LUCIDE_ICONS, LucideIconProvider } from 'lucide-angular';
+import { I18n } from './services/i18n';
 
 const lucideIcons = {
   ArrowLeft,
@@ -35,6 +42,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+
+    provideAppInitializer(() => inject(I18n).init()),
 
     // Lucide icons – global registration
     { provide: LUCIDE_ICONS, multi: true, useValue: new LucideIconProvider(lucideIcons) },
