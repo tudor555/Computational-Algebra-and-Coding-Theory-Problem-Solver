cc4fd58ddb6750faa68559d03e08e476d8fe338a
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-07 19:25:59 +0200

    feat: change footer for accept multilanguage

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { TranslatePipe } from '../../pipes/translate';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, LucideAngularModule],
+  imports: [RouterLink, LucideAngularModule, TranslatePipe],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
