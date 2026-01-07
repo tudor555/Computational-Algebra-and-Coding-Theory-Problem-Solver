7ed66e9fa1dc6fa5e84d7cea425c1af8e78503ab
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-07 16:46:28 +0200

    style: made design of footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+  readonly currentYear = new Date().getFullYear();
 }
