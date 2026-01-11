e5f58c7d00392205a475cba015f03acec196317e
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-11 17:22:35 +0200

    feat: generate component for problem 7

---
+++ src/app/pages/problems/problem7-m2r-matrix-order-idempotent/problem7-m2r-matrix-order-idempotent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem7-m2r-matrix-order-idempotent',
+  imports: [],
+  templateUrl: './problem7-m2r-matrix-order-idempotent.html',
+  styleUrl: './problem7-m2r-matrix-order-idempotent.scss',
+})
+export class Problem7M2rMatrixOrderIdempotent {
+
+}
