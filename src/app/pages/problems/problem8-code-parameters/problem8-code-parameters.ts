c9669ac1e469c7213cdfc61891ba6f0da2abb527
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-11 17:23:07 +0200

    feat: generate component for problem 8

---
+++ src/app/pages/problems/problem8-code-parameters/problem8-code-parameters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem8-code-parameters',
+  imports: [],
+  templateUrl: './problem8-code-parameters.html',
+  styleUrl: './problem8-code-parameters.scss',
+})
+export class Problem8CodeParameters {
+
+}
