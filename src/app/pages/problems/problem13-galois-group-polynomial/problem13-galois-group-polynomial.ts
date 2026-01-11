682450e3e168eed173d8c9c572a9e888d0d2a4b4
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-11 17:21:02 +0200

    feat: generate component for problem 13

---
+++ src/app/pages/problems/problem13-galois-group-polynomial/problem13-galois-group-polynomial.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem13-galois-group-polynomial',
+  imports: [],
+  templateUrl: './problem13-galois-group-polynomial.html',
+  styleUrl: './problem13-galois-group-polynomial.scss',
+})
+export class Problem13GaloisGroupPolynomial {
+
+}
