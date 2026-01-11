231713df422e6a578f61ab3d7ba70c078f8fb11f
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-11 17:20:36 +0200

    feat: generate component for problem 10

---
+++ src/app/pages/problems/problem10-generator-matrix-codewords/problem10-generator-matrix-codewords.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem10-generator-matrix-codewords',
+  imports: [],
+  templateUrl: './problem10-generator-matrix-codewords.html',
+  styleUrl: './problem10-generator-matrix-codewords.scss',
+})
+export class Problem10GeneratorMatrixCodewords {}
