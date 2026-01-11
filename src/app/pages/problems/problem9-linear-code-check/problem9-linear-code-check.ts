32a94b8364818749c1fedc4749da3689c37ca8c6
Tudor_Bumb <bumbtudor03@gmail.com>
2026-01-11 17:19:53 +0200

    feat: generate component for problem 9

---
+++ src/app/pages/problems/problem9-linear-code-check/problem9-linear-code-check.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem9-linear-code-check',
+  imports: [],
+  templateUrl: './problem9-linear-code-check.html',
+  styleUrl: './problem9-linear-code-check.scss',
+})
+export class Problem9LinearCodeCheck {
+
+}
